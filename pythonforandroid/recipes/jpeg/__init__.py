c98abd2b79e60bd5499466b139189da89c50b714
liu.sh <liu.sh@reachauto.com>
2025-05-14 15:45:40 +0800

    更新Pillow配方版本至11.1.0，修复setup.py中的WEBP_ROOT设置，确保构建过程中的依赖库正确链接，同时在jpeg配方中添加CMake政策版本设置。

--- pythonforandroid/recipes/jpeg/__init__.py
+++ pythonforandroid/recipes/jpeg/__init__.py
@@ -48,6 +48,7 @@ class JpegRecipe(Recipe):
                     # Force disable shared, with the static ones is enough
                     '-DENABLE_SHARED=0',
                     '-DENABLE_STATIC=1',
+                    '-DCMAKE_POLICY_VERSION_MINIMUM=3.5',
                     _env=env)
             shprint(sh.make, _env=env)
 
